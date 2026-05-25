66d440d7cb26dacc7cf51553a7feefb266900468
rajshankarbalasubramanian <rajshankarsubramaniam@gmail.com>
2026-05-25 22:23:07 +0530

    Add production settings for Django application

---
+++ production.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['your-production-domain.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
